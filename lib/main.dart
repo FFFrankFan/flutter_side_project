beacd47b6b43eeea73c0a4a1d0df227177f72921
FFFrankFan <b98302218@gmail.com>
2024-05-31 15:20:05 +0800

    firebase init and deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter_project/open_ai_service.dart';
 
-void main() {
+Future<void> main() async {
   // 初始化 Firebase
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const MyApp());
 }
